bf0d4ba7647831a9f0c3ef9c81c7bb6e09f0d41e
Farid Tavakoli <fst@sc.edu>
2025-07-28 12:07:57 -0400

    I added a mysql database (Movie) to store the user input in so that when the server is turned off and on again. The users data is saved. This means if you add a movie it will stay on the screen until you delete it.

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from mysql.connector import Error
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__, static_url_path='/static')
+cnx = mysql.connector.connect(user='root',
+                                  password='Soccer12345!',
+                                  host='localhost',
+                                  database='Movie')
 
-
-movies = []
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,20 +15,32 @@ def index():
 @app.route('/movie', methods=['POST'])
 def movie():
     data = request.get_json()
-    new_movie = data['movie_content']
-    movies.append(new_movie)
-    return new_movie
+    if not data or 'movieName' not in data:
+        return jsonify({"error": "movieName is required"}), 400 #this is to see what the heck the problem is
+    
+    new_movie = data['movieName']
+    cursor = cnx.cursor()
+    cursor.execute("INSERT INTO movies (movieName) VALUES (%s)", (new_movie,))
+    cnx.commit()
+    cursor.close()
+    return jsonify({"message": "Movie added successfully"}), 201
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM movies")
+    movies = cursor.fetchall()
+    cursor.close()
     return jsonify(movies)
 
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
 def delete_movie(movie_id):
-    if movie_id < 0 or movie_id >= len(movies):
-        return jsonify({"error": "Movie not found"}), 404
-    movies.pop(movie_id)
+    cursor = cnx.cursor()
+    cursor.execute("DELETE FROM movies WHERE id = %s", (movie_id,))
+    cnx.commit()
+    cursor.close()
     return '', 204
+        
 
 # ---Custom Error pages---
 #invalid link
